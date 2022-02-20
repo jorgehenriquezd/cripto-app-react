f270c53c3a7e2555788c852f9c17e69914c1a570
Jorge Henriquez <jorgehenriquez1711@gmail.com>
2022-02-20 13:34:56 -0400

    Ajustes

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -39,7 +39,7 @@ const Cryptocurrencies = ({ simplified }) => {
             <Link to={`/crypto/${currency.uuid}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
-                extra={<img className='crypto-image' src={currency.iconUrl} />}
+                extra={<img className='crypto-image' alt='icon' src={currency.iconUrl} />}
                 hoverable
               >
                 <p>Price: {millify(currency.price)}</p>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -47,7 +47,7 @@ const News = ({ simplified }) => {
             <a href={news.url} target="_blank" rel='noreferrer'>
               <div className='news-image-container'>
                 <Title className='news-title' level={4}>{news.name}</Title>
-                <img src={news?.image?.thumbnail?.contentUrl || demoImage}  />              
+                <img alt='thumbnail' src={news?.image?.thumbnail?.contentUrl || demoImage}  />              
               </div>
               <p> {news.description > 100 ? `${news.description.substring(0,100)}...` : news.description} </p>
 
